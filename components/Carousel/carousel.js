17324832c463ad2f4d0d8e6dadbdca8d60e935dc
magusin <fanatsy4@gmail.com>
2023-10-04 17:09:38 +0200

    add algorithme for carousel

--- components/Carousel/carousel.js
+++ components/Carousel/carousel.js
@@ -2,116 +2,118 @@ import React from 'react'
 import styles from './carousel.module.css'
 
 const VideoCarousel = () => {
-  const [rotateValue, setRotateValue] = React.useState(0);
-  const carouselRef = React.useRef(null);
+  const carouselRef = React.useRef(null)
 
+  const numberOfVideos = 6
+  const angle = 360 / numberOfVideos
+  const [rotateValue, setRotateValue] = React.useState(0)
+  const radius = 400
   const handleNext = () => {
-    setRotateValue(prev => prev - 30);
+    setRotateValue((prev) => prev - angle)
   }
 
   const handlePrev = () => {
-    setRotateValue(prev => prev + 30);
+    setRotateValue((prev) => prev + angle)
   }
 
   React.useEffect(() => {
     if (carouselRef.current) {
-      const carousel = carouselRef.current;
-      carousel.style.transform = `rotateY(${rotateValue}deg)`;
+      const carousel = carouselRef.current
+      carousel.style.setProperty('--angle', angle)
+      carousel.style.transform = `rotateY(${rotateValue}deg)`
     }
-  }, [rotateValue]);
+  }, [angle, rotateValue])
 
   return (
     <>
-    <div className={styles.carouselContainer}>
-    <div ref={carouselRef} className={styles.carousel}>
-      <div className={`${styles.item} ${styles.a}`}>
-          <iframe
-            src="https://www.youtube.com/embed/PExjHWbkafk"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className={`${styles.item} ${styles.b}`}>
-          <iframe
-            src="https://www.youtube.com/embed/difKLkqME4I"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className={`${styles.item} ${styles.c}`}>          
-        <iframe
-            src="https://www.youtube.com/embed/ER7DFORQVt0"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className={`${styles.item} ${styles.d}`}>          
-        <iframe
-            src="https://www.youtube.com/embed/ER7DFORQVt0"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className={`${styles.item} ${styles.e}`}>
-          <iframe
-            src="https://www.youtube.com/embed/ER7DFORQVt0"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className={`${styles.item} ${styles.f}`}>
-          <iframe
-            src="https://www.youtube.com/embed/ER7DFORQVt0"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className={`${styles.item} ${styles.g}`}>
-          <iframe
-            src="https://www.youtube.com/embed/PExjHWbkafk"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className={`${styles.item} ${styles.h}`}>
-          <iframe
-            src="https://www.youtube.com/embed/difKLkqME4I"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className={`${styles.item} ${styles.i}`}>          
-        <iframe
-            src="https://www.youtube.com/embed/ER7DFORQVt0"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className={`${styles.item} ${styles.j}`}>          
-        <iframe
-            src="https://www.youtube.com/embed/ER7DFORQVt0"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className={`${styles.item} ${styles.k}`}>
-          <iframe
-            src="https://www.youtube.com/embed/ER7DFORQVt0"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className={`${styles.item} ${styles.l}`}>
-          <iframe
-            src="https://www.youtube.com/embed/ER7DFORQVt0"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
+      <div className={styles.carouselContainer}>
+        <div ref={carouselRef} className={styles.carousel}>
+          <div
+            style={{
+              '--index': 0,
+              transform: `rotateY(${angle * 0}deg) translateZ(${radius})`
+            }}
+            className={`${styles.item} ${styles.a}`}
+          >
+            <iframe
+              src="https://www.youtube.com/embed/PExjHWbkafk"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div
+            style={{
+              '--index': 1,
+              transform: `rotateY(${angle * 1}deg) translateZ(${radius})`
+            }}
+            className={`${styles.item} ${styles.b}`}
+          >
+            <iframe
+              src="https://www.youtube.com/embed/difKLkqME4I"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div
+            style={{
+              '--index': 2,
+              transform: `rotateY(${angle * 2}deg) translateZ(${radius})`
+            }}
+            className={`${styles.item} ${styles.c}`}
+          >
+            <iframe
+              src="https://www.youtube.com/embed/ER7DFORQVt0"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div
+            style={{
+              '--index': 3,
+              transform: `rotateY(${angle * 3}deg) translateZ(${radius})`
+            }}
+            className={`${styles.item} ${styles.d}`}
+          >
+            <iframe
+              src="https://www.youtube.com/embed/PExjHWbkafk"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div
+            style={{
+              '--index': 4,
+              transform: `rotateY(${angle * 4}deg) translateZ(${radius})`
+            }}
+            className={`${styles.item} ${styles.e}`}
+          >
+            <iframe
+              src="https://www.youtube.com/embed/difKLkqME4I"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div
+            style={{
+              '--index': 5,
+              transform: `rotateY(${angle * 5}deg) translateZ(${radius})`
+            }}
+            className={`${styles.item} ${styles.f}`}
+          >
+            <iframe
+              src="https://www.youtube.com/embed/ER7DFORQVt0"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
       </div>
-    </div>
-    <div className={styles.next} onClick={handleNext}>Next</div>
-    <div className={styles.prev} onClick={handlePrev}>Prev</div>
+      <div className={styles.next} onClick={handleNext}>
+        Next
+      </div>
+      <div className={styles.prev} onClick={handlePrev}>
+        Prev
+      </div>
     </>
   )
 }
