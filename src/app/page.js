46c2b216d267b37f77a030407d0365f12ca0f212
magusin <fanatsy4@gmail.com>
2023-10-05 19:32:05 +0200

    add favicon

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,27 @@
 'use client'
-import React from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './page.module.css';
-import Carousel from '#/components/Carousel/carousel';
+import React from 'react'
+import Image from 'next/image'
+import styles from './page.module.css'
+import Carousel from '#/components/Carousel/carousel'
 
 export default function Home() {
-
-
   return (
-   <div>
-      <Head>
-        <title>Re: Take</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    <div>
       <main className="mainContent">
-        <Image src="/logo.png" alt="Re Take Logo" width="150" height="150" />
-    
+        <Image
+          priority
+          src="/logo.png"
+          alt="Re: Take Logo"
+          width="150"
+          height="150"
+        />
+
         <Carousel />
-  
-        
-       
       </main>
 
       <footer className="footer">
         <p>&copy; 2023 Re: Take</p>
       </footer>
-      </div>
+    </div>
   )
 }
