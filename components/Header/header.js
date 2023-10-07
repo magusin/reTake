4b2dc40891b7102c21a8625799f2dba8c7660034
magusin <fanatsy4@gmail.com>
2023-10-07 12:16:54 +0200

    add navigation

--- components/Header/header.js
+++ components/Header/header.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import styles from './header.module.css';
+import React from 'react'
+import styles from './header.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-      <Image
+        <Link href="/">
+        <Image
           priority
           src="/logo.png"
           alt="Re: Take Logo"
           width="80"
           height="80"
         />
+        </Link>
       </div>
       <nav className={styles.nav}>
-        <a href="#" className={styles.link}>Accueil</a>
-        <a href="#" className={styles.link}>À propos</a>
-        <a href="#" className={styles.link}>Services</a>
-        <a href="#" className={styles.link}>Contact</a>
+        <Link href="/" className={styles.link}>Accueil
+        </Link>
+        <Link href="/about" className={styles.link}>À propos
+        </Link>
+        <Link href="/contact" className={styles.link}>Contact
+        </Link>
       </nav>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
